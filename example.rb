5e548deaa2af80eae6b5ddb968e410e3ec8ada16
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-08-13 10:23:13 -0400

    Updated person class

--- example.rb
+++ example.rb
@@ -1,13 +1,20 @@
 class Person
-  def initialize(name)
-    @name = name
+  attr_accessor :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
   end
 
-  def name
-    name
+  def first_name
+    first_name
   end
 
-  def name=(name)
-    self.name = name
+  def first_name=(first_name)
+    self.first_name = first_name
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
   end
 end
